7728b791bfdffcd7f2498a508427729fd9c7b1ec
Beau Roels <beauroels@hotmail.be>
2021-11-30 11:10:56 +0100

    Acl's toevoegen
    Toevoegen van de juiste rechten
    ReadAndExecute voor de leerlingen
    Fullacces voor de leerkrachten
    
    de acl uitvoeren op de correcte folder
    
    Als de folder niet bestaat, wordt de folder gemaakt
    
    een AD group maken voor de klassgroep

--- Scriptpowershell.ps1
+++ Scriptpowershell.ps1
@@ -26,13 +26,55 @@ Function CreateGroups()
     # Looping through the records
     Foreach ($ClassRecord in $Datatable)
     {
+      # Get the data from the 'klas' record
       $GroupName = $ClassRecord.klas
 
-      # Creating the container
-      New-ADGroup -Name $GroupName -SamAccountName $GroupName -GroupCategory Security -GroupScope Global -DisplayName $GroupName -Path 'OU=leerlingen,DC=Handelsschoolaalst,DC=be' -Description "Leerlingen in deze groep maken deel uit van klas $ClassName"    }
+      # Create a new AD Group for the class
+      New-ADGroup -Name $GroupName -SamAccountName $GroupName -GroupCategory Security -GroupScope Global -DisplayName $GroupName -Path 'OU=leerlingen,DC=Handelsschoolaalst,DC=be' -Description "Leerlingen in deze groep maken deel uit van klas $ClassName"
+
+      # [ Folder creation ]
+      # Test if the remote folder exists
+      $FolderExists = Test-Path -Path "\\FS-LLN\leerlingen\$ClassName"
+      If (-not $FolderExists)
+      {
+        # If the class folder doesn't exist we create it  
+        New-Item -Path '\\FS-LLN\leerlingen\' -Name $ClassName -ItemType 'Directory'
+      }
+
+      # Test if the remote folder exists
+      $FolderExists = Test-Path -Path "\\FS-LLN\leerlingen\$ClassName\Opdrachten"
+      If (-not $FolderExists)
+      {
+        # If the opdrachten folder doesn't exist we create it  
+        New-Item -Path "\\FS-LLN\leerlingen\$ClassName" -Name 'Opdrachten' -ItemType 'Directory'
+      }
+
+      # [ ACL rights for the folder ]
+      # Update ACL policy of Opdrachten folder
+      $ACLOpdrachten = Get-Acl -Path "\\FS-LLN\leerlingen\$ClassName\Opdrachten"
+
+      # User that gains rights
+      $ACLIdentity = "DeHandelsschool\$ClassName"
+
+      # The rights that will be given
+      $ACLRights = 'ReadAndExecute'
+
+      # Allow/Deny the ACL rules
+      $ACLType = 'Allow'
+
+      # Create the ACL object using the arguments set by the variables
+      $ACLArguments = $ACLIdentity, $ACLRights, $ACLType
+      $ACLAccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $ACLArguments
+
+      # Apply the ACL rule and set it to the \\FS-LLN\$ClassName\Opdrachten folder
+      $ACLOpdrachten.SetAccessRule($fileSystemAccessRule)
+      Set-Acl -Path "\\FS-LLN\leerlingen\$ClassName\Opdrachten" -AclObject $NewAcl
+    }
   }
   Catch [System.Data.OleDb.OleDbException]
   {
     Write-Host 'Cannot find record'
   }
+
+  $DbConnection.Close()
 }
