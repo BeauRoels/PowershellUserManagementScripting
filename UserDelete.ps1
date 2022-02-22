b646578fe47c8b79a1c662af9f93018f42d255ca
BeauRoels <62283844+BeauRoels-Valid@users.noreply.github.com>
2022-02-22 15:04:16 +0100

    fixed user delete

--- UserDelete.ps1
+++ UserDelete.ps1
@@ -1,5 +1,11 @@
 
-
+$FileserverLLN = "\\FS-LLN"
+$FileserverPERS = "\\FS-Personeel"
+$DC = "DC=Handelsschoolaalst, DC=be"
+$Domain = "DeHandelsschool"
+$MainGroup = "Leerlingen"
+$MainFolder= "leerlingen"
+$Moderator= "leerkrachten"
 
 $FileserverLLN = "\\WIN-8KV6AGI5ESN"
 $FileserverPERS = "\\WIN-8KV6AGI5ESN"
@@ -12,9 +18,9 @@ $Moderator= "leerkracht"
 $DC = "DC=TEST, DC=be"
 
 #delete users
+
  Function DeleteLeerlingen
  {
-   Write-Host "[WARNING You are about to delete ALL users!]"
    Write-Host "Deleting all users in Leerlingen"
    Get-ADOrganizationalUnit -LDAPFilter '(name=*)' -SearchBase "OU=Leerlingen, $DC" -SearchScope OneLevel | Remove-ADOrganizationalUnit -Recursive
    Write-Host "Users deleted..."
@@ -22,7 +28,7 @@ $DC = "DC=TEST, DC=be"
  Function DeleteLeerlingenDirectory
  {
     Write-Host "Deleting all Folders in Leerlingen Directory"
-    Get-ChildItem "$FileserverLLN\$MainFolder\*" Remove-Item -Recurse -Force 
+    Get-ChildItem "$FileserverLLN\$MainFolder" | Remove-Item -Recurse -Force 
     Write-Host "Folders deleted..."
  }
  DeleteLeerlingen
